961a2b2d65b199d2f83fd47b304477a6a494a079
Philipp Frenzel <pf@com-x-cha.com>
2022-03-14 16:06:08 +0100

    Update __init__.py

--- eventbookingmgr/eventbooking-scan/__init__.py
+++ eventbookingmgr/eventbooking-scan/__init__.py
@@ -7,5 +7,5 @@ from services.eventbooking_service import EventBooking
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    data = get_todos()
-    return func.HttpResponse(json.dumps(data), headers={"content-type": "application/json"})
+    eventBooking = EventBooking("DataProduct1")
+    return func.HttpResponse(json.dumps(eventBooking), headers={"content-type": "application/json"})
