11d0a00aeee71a3b860197a79ee0b478fa4fa63f
philippfrenzel <philipp@frenzel.net>
2022-03-21 15:26:11 +0100

    adding first draft for keyvault service capsule

--- eventbookingmgr/services/keyvault_service.py
+++ eventbookingmgr/services/keyvault_service.py
@@ -8,4 +8,14 @@ class azKeyVault:
         keyVaultName: str
     ) -> None:
         self.keyVaultName = keyVaultName
+        keyVaultName = os.environ["KEY_VAULT_NAME"]
+        KVUri = f"https://{keyVaultName}.vault.azure.net"
 
+  def getSecret(
+      self,
+      secretName: str
+    ) -> str:
+      credential = DefaultAzureCredential()
+      client = SecretClient(vault_url=self.KVUri, credential=credential)
+      retrieved_secret = client.get_secret(secretName)
+      return secretName
