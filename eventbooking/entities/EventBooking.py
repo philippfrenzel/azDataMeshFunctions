bec1ae556e543e397e5719af6939ad5a7a04db1d
philippfrenzel <philipp@frenzel.net>
2022-03-30 09:29:53 +0200

    add validation

--- eventbooking/entities/EventBooking.py
+++ eventbooking/entities/EventBooking.py
@@ -2,6 +2,8 @@ import pyodbc
 import os
 import datetime
 from azure.identity import DefaultAzureCredential
+from jsonschema import validators, Draft4Validator, FormatChecker
+from jsonschema.exceptions import ValidationError
 from sqlalchemy import create_engine
 from sqlalchemy import Table, MetaData, Column, Integer, String, DateTime
 from sqlalchemy.engine import URL
@@ -29,5 +31,52 @@ class EventBooking(Base):
   
   def __repr__(self):
         return f"Dataproduct(RootDataproduct={self.FKDataproductId!r}, relation={self.Relation!r}, ChildDataproduct={self.DataproductId!r})"
+
+  def validate_json(self,json):
+     schema = {
+        'type': 'object',
+        'properties': {
+            'value': {
+                'type': 'float',
+                'format': 'even',
+                'is_positive': True
+            },
+          }
+        }
+     
+     # Define custom validators. Each must take exactly 4 arguments as below.
+     def is_positive(validator, value, instance, schema):
+        if not isinstance(instance, Number):
+            yield ValidationError("%r is not a number" % instance)
+        
+        if value and instance <= 0:
+            yield ValidationError("%r is not positive integer" % (instance,))
+        elif not value and instance > 0:
+            yield ValidationError("%r is not negative integer nor zero" % (instance,))
+     
+     # Add your custom validators among existing ones.
+     all_validators = dict(Draft4Validator.VALIDATORS)
+     all_validators['is_positive'] = is_positive
+
+     # Create a new validator class. It will use your new validators and the schema
+     # defined above.
+     MyValidator = validators.create(
+        meta_schema=Draft4Validator.META_SCHEMA,
+        validators=all_validators
+     )
+
+     # Create a new format checker instance.
+     format_checker = FormatChecker()
+     # Register a new format checker method for format 'even'. It must take exactly one
+     # argument - the value for checking.
+     @format_checker.checks('even')
+     def even_number(value):
+        return value % 2 == 0
+     # Create a new instance of your custom validator. Add a custom type.
+     my_validator = MyValidator(
+        schema, types={"float": float}, format_checker=format_checker
+     )
+
+     return my_validator.validate(json)
 
 Base.metadata.create_all(engine)
